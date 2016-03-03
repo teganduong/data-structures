8363cbb5223ea6d21ab2a2c8d3cddafa84e1f42f
teganduong <teganduong@gmail.com>
2016-03-02 17:21:12 -0800

    Complete stack of functional-shared pattern to pass all specs

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.items = 0;
+
+  //someInstance.push = stackMethods.push;
+  //someInstance.pop = stackMethods.pop;
+  //someInstance.size = stackMethods.size;
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.items] = value;
+  this.items++;
+};
 
+stackMethods.pop = function() {
+  this.items--;
+  return this.storage[this.items];
+};
+
+stackMethods.size = function() {
+  return Math.max(0, this.items);
+};
