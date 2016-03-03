ce8348bc20d4bb0a8b19663959cc97ce7a7427bc
Tegan Duong <teganduong@gmail.com>
2016-03-02 20:28:37 -0800

    Complete stack function of pseudoclassical pattern

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,23 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.storage = {};
+  this.items = 0;
 };
 
 
+
+Stack.prototype.push = function(value) {
+  this.storage[this.items] = value;
+  this.items++;
+};
+
+Stack.prototype.pop = function() {
+  this.items--;
+  return this.storage[this.items];
+};
+
+Stack.prototype.size = function() {
+  return Math.max(0, this.items);
+};
