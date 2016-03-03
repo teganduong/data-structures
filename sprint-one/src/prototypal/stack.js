93142dcb4fc83c6dcd028606c97589616b5ff3f9
teganduong <teganduong@gmail.com>
2016-03-02 18:21:07 -0800

    Implement code to pass specs of stack shared behavior in prototypal pattern

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+var someInstance = Object.create(Stack.prototype);
+  someInstance.storage = {};
+  someInstance.items = 0;
+
+  //someInstance.push = stackMethods.push;
+  //someInstance.pop = stackMethods.pop;
+  //someInstance.size = stackMethods.size;
+
+  return someInstance;
 };
 
 var stackMethods = {};
 
+Stack.prototype.push = function(value) {
+  this.storage[this.items] = value;
+  this.items++;
+};
 
+Stack.prototype.pop = function() {
+  this.items--;
+  return this.storage[this.items];
+};
+
+Stack.prototype.size = function() {
+  return Math.max(0, this.items);
+};
