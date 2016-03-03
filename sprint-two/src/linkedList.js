5d203e78b84c7f8849d4c5008db35cc93879126e
teganduong <teganduong@gmail.com>
2016-03-03 11:28:33 -0800

    Complete linkedList.js to pass all specs

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,13 +4,37 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //check if node exists
+    var newNode = Node(value);
+
+    if (list.tail !== null) {
+      list.tail.next = newNode;
+    } else {
+      list.head = newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    if (list.head !== null) {
+      var temp = list.head;
+    }
+    list.head = list.head.next; 
+    return temp.value;
   };
 
   list.contains = function(target) {
+    var containhead = list.head;
+    var result = false;
+    while (containhead !== null) {
+      if (containhead.value === target) {
+        result = true;
+      }
+      containhead = containhead.next;
+    }
+    return result;
   };
+
 
   return list;
 };
