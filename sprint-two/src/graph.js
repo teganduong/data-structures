af3dbee9b0a21c0fddf75b64f192bd9a48b3980e
teganduong <teganduong@gmail.com>
2016-03-03 20:08:48 -0800

    Implement addNode and removeNode functions to pass specs

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,31 +3,53 @@
 // ------------------------
 // Instantiate a new graph
 var Graph = function() {
+ this.graphNodes = {};
+ 
 };
 
+// var Node = function(value) {
+//   var node = {};
+
+//   node.value = value;
+//   node.next = null;
+
+//   return node;
+// };
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  
+  this.graphNodes.value = node;
+
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  if (this.graphNodes.value === node) {
+    return true;
+  }
+  return false;
 };
 
 // ------------------------
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  this.graphNodes.value = undefined;
 };
 
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+ 
 };
 
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+ 
+  this.graphNodes.loc.tovalue.push(toNode);
+
 };
 
 // ------------------------
@@ -43,5 +65,5 @@ Graph.prototype.forEachNode = function(cb) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
+var graph = new Graph();
 
