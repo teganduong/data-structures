6be1fd3b56b9a89d55fccca9a9c94574ce940dac
teganduong <teganduong@gmail.com>
2016-03-04 12:31:40 -0800

    Update hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,18 +3,25 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.arrayVals = [];
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.array = [];
+  this._storage.array[0] = k;
+  this._storage.array[1] = v;
+  this._storage.set(index, this._storage.array);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this.arrayVals[index + 1] = undefined;
 };
 
 
