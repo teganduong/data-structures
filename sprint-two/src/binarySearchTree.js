55075aab0f6df4211437f755117b35d74805dba6
teganduong <teganduong@gmail.com>
2016-03-05 11:29:02 -0800

    Complete binarySearchTree to pass all specs

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,59 @@
 var BinarySearchTree = function(value) {
+  var node = Object.create(treeMethods);
+  node.value = value;
+  node.left = null;
+  node.right = null;
+
+  return node;
 };
 
+var treeMethods = {};
+
+treeMethods.insert = function(value) {
+  // create a instance node with the value
+  var currentNode = BinarySearchTree(value);
+  //Compare the root node value and the current value
+  if (this.value > currentNode.value ) {
+    if (this.left === null) {
+      this.left = currentNode;
+    } else {
+      this.left.insert(value); 
+    }
+  } else if (this.value < currentNode.value) {
+    if (this.right === null) {
+      this.right = currentNode;
+    } else {
+      this.right.insert(value); 
+    }
+  }
+
+};
+
+treeMethods.contains = function(value) {
+  var nodevalue = this.value;
+  var result = false;
+  //base case check value with nodevalue
+  if (nodevalue === value) {
+    result = true;
+  } else if ( this.left && nodevalue > value) {
+    result = this.left.contains(value);
+  } else if ( this.right && nodevalue < value) {
+    result = this.right.contains(value);
+  } 
+  return result;
+
+};
+
+treeMethods.depthFirstLog = function(cb) {
+  cb(this.value);
+  if (this.left) {
+    this.left = this.left.depthFirstLog(cb);
+  } 
+  else if (this.right) {
+    this.right = this.right.depthFirstLog(cb);
+  }
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
